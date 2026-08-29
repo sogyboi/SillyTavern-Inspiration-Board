e3af52ba17094fc14c1295cd24fb7fcc69c0e87f
sogyboi <92685940+sogyboi@users.noreply.github.com>
2026-08-29 09:47:51 -0700

    Configure Capture Browser Android app

---
+++ android-companion/app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.sogyboi.inspirationboard.capture"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.sogyboi.inspirationboard.capture"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 5
+        versionName = "0.5.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
